c329ae2782060ccbdda3c7a65d5587f658a924c6
Abdulwahab-Alawadhi <abdulwahab.alawadhi56@gmail.com>
2024-11-05 14:04:49 +0300

    done

--- src/IphoneSettings.java
+++ src/IphoneSettings.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
-
 public class IphoneSettings {
+
+    private static IphoneSettings instance;
+
+    private int brightness;
+    private boolean wifi;
+
+    private IphoneSettings() {
+        brightness = 100;
+        wifi = false;
+    }
+
+    public static IphoneSettings getInstance() {
+        if (instance == null) {
+            instance = new IphoneSettings();
+        }
+        return instance;
+    }
+
+
+    public void disableWifi() {
+
+        wifi = false;
+
+    }
+
+    public void enableWifi() {
+
+        wifi = true;
+    }
+
+    public int getBrightness() {
+        return brightness;
+    }
+
+    public void setBrightness(int brightness) {
+        this.brightness = brightness;
+    }
+
+    @Override
+    public String toString() {
+        return "IphoneSettings{" +
+                "brightness=" + brightness +
+                ", wifi=" + wifi +
+                '}';
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        IphoneSettings iphoneSettings = IphoneSettings.getInstance();
+        IphoneSettings iphoneSettings2 = IphoneSettings.getInstance();
+
+
+        System.out.println("1: " + iphoneSettings);
+        System.out.println("2: " + iphoneSettings2);
+
+        iphoneSettings.enableWifi();
+        iphoneSettings2.setBrightness(5);
+
+        System.out.println("1: " + iphoneSettings);
+        System.out.println("2: " + iphoneSettings2);
+
+
     }
 }
